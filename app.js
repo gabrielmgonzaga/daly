a2d428fe77f611eb7ed4d6cc6650368efcf6b631
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2017-01-22 04:09:46 -0800

    Remove postgresql.

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
 const express = require('express')
 const secret = require('./config')
 const request = require('request')
-const bodyParser = require('body-parser')
 
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host: 'localhost',
-    user: 'superuser',
-    database: 'weather'
-  }
-})
 
 const app = express()
 
 app.use(express.static('public'))
-app.use(bodyParser.json())
 
 const API_KEY = secret.key
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
@@ -30,45 +20,6 @@ app.get('/weather/:city', (req, res) => {
       res.json(body)
     }
   })
-})
-
-// Saved cities route
-app.get('/saved/cities', (req, res) => {
-  knex
-    .select()
-    .table('cities')
-    .then(cities => {
-      const promises = cities.map(city => {
-        return new Promise((resolve, reject) => {
-          const url = `${ROOT_URL}&q=${city.name},us`
-          request(url, { json: true }, (error, response, body) => {
-            if (error) reject(error)
-            resolve(Object.assign({}, body, { cityId: city.id }))
-          })
-        })
-      })
-      Promise.all(promises)
-        .then(data => res.json(data))
-    })
-})
-
-// Save route
-app.post('/save', (req, res) => {
-  knex
-    .table('cities')
-    .insert(req.body)
-    .into('cities')
-    .returning(['id', 'name'])
-    .then(city => res.json(city[0]))
-})
-
-// Delete route
-app.delete('/city/:id', (req, res) => {
-  knex
-    .table('cities')
-    .where('id', req.params.id)
-    .del()
-    .then(res.sendStatus(204))
 })
 
 // Server
